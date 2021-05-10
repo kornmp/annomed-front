64945684a5d04ae88ba15a4271be8ae1a0dfff35
Korn Piboonsirikul <korn@everapp.io>
2021-05-10 16:04:27 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
-import ClientPlatform from './pages/ClientPlatform';
-import RegistrationWorker from './pages/RegistrationWorker';
-import RegistrationClient from './pages/RegistrationClient';
-import LoginWorker from './pages/LoginWorker';
-import LoginClient from './pages/LoginClient';
 
 const App = () => {
 
@@ -15,11 +10,6 @@ const App = () => {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/ClientPlatform" component={ClientPlatform} />
-          <Route path="/LoginWorker" component={LoginWorker} />   
-          <Route path="/LoginClient" component={LoginClient} />
-          <Route path="/RegistrationWorker" component={RegistrationWorker} />
-          <Route path="/RegistrationClient" component={RegistrationClient} />
         </Switch>
       </Router>
   );
--- src/constants/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_ALL = 'FETCH_ALL';
-export const CREATE = 'CREATE';
-
-export const AUTH = 'AUTH';
-export const LOGOUT = 'LOGOUT';
